b62789ffb573f21919cb6ba39a10835978710d51
emd5 <emahoney5@mail.greenriver.edu>
2018-08-26 17:08:44 -0700

    add test files, update pipenv environment, implemenet stack and queue methods respectively

--- data_structures/stack/stack.py
+++ data_structures/stack/stack.py
@@ -3,26 +3,34 @@ from .node import Node
 
 class Stack(object):
     def __init__(self):
+        """An instance method of the stack object. """
         self.top: Node = None
         self._length: int = 0
 
     def __str__(self):
-        pass
+        """A string representation of the stack object. """
+        return f'Top: {self.top} | Length: {self._length}'
 
     def __repr__(self):
-        pass
+        """An 'official' string reputation of the stack object. """
+        return f'<Stack| Top: {self.top} | Length: {self._length}'
 
     def __len__(self):
-        pass
+        """The length of the stack"""
+        return self._length
 
-    def push(self, val):
-        """Which takes any value as an argument and adds that value to the top of the stack"""
-        pass
+    def push(self, node):
+        """Which takes any value as an argument and adds that value to the top of the stack. """
+        node._next = self.top
+        self.top =node
 
     def pop(self):
-        """Which takes no arguments and removes / returns the Node at the top of the stack"""
-        pass
+        """Which takes no arguments and removes / returns the Node at the top of the stack. """
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        return temp
 
     def peek(self):
-        """Which takes no arguments and returns the Node at the top of the stack"""
-        pass
+        """Which takes no arguments and returns the Node at the top of the stack. """
+        return self.top
