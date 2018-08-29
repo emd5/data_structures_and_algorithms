56ad39cf51c14c739abe8709b9f1519015bce57e
emd5 <emahoney5@mail.greenriver.edu>
2018-08-29 05:59:53 -0700

    reimplemented methods, test are not working

--- challenges/fifo_animal_shelter/testing_animal_shelter.py
+++ challenges/fifo_animal_shelter/testing_animal_shelter.py
@@ -2,12 +2,25 @@ from .fifo_animal_shelter import AnimalShelter
 import pytest
 
 
+@pytest.fixture
+def load_dog():
+    dog_queue = AnimalShelter()
+    dog_queue.enqueue('dog')
+    return dog_queue.front
+
+
 @pytest.fixture()
-def cat_queue()
+def load_cats():
+    cat_queue = AnimalShelter()
+    cat_queue.enqueue('cat')
+    return cat_queue.front
+
+
+def test_animal_shelter_class_exist():
+    """Test animal shelter class exist. """
+    assert AnimalShelter
 
 
 
 
-@pytest.fixture()
-def dog_queue():
-    pass
+
