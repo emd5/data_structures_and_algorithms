e9d8c6c0e0038bff48c2c63ebaf244fd419916d4
emd5 <emahoney5@mail.greenriver.edu>
2018-09-04 13:55:01 -0700

    update test methods

--- data_structures/binary_tree/test_bst.py
+++ data_structures/binary_tree/test_bst.py
@@ -3,12 +3,14 @@ import pytest
 
 
 def test_empty_bt():
+    """Test an instance of an empty binary tree. """
     bt = BinaryTree()
     assert isinstance(bt, BinaryTree)
     assert bt.root is None
 
 
 def test_insert_into_empty_bt():
+    """Test insert one node to a tree. """
     bt = BinaryTree()
     assert bt.root is None
     bt.insert(25)
@@ -32,6 +34,31 @@ def test_inorder_traversal():
 
     bt.in_order(generate_list)
     assert expected == actual
+
+
+def test_preorder_traversal():
+    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    bt.in_order(generate_list)
+    assert expected == actual
+
+
+def test_postorder_traversal():
+    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    bt.in_order(generate_list)
+    assert expected == actual
+
 
 
 def test_insert_value_that_already_exists():
