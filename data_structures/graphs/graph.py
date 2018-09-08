2c8c08295d17a04efecba680689c331983d9d280
emd5 <emahoney5@mail.greenriver.edu>
2018-09-07 21:13:01 -0700

    implement graph class with all passed test cases

--- data_structures/graphs/graph.py
+++ data_structures/graphs/graph.py
@@ -1,15 +1,15 @@
 class Graph:
     def __init__(self):
-        """Set up an empty graph"""
+        """Constructs an empty graph"""
         self.graph = {}
 
     def __repr__(self):
         """Set up a graph with vertices"""
-        pass
+        return f'Graph: {self.graph}'
 
     def __str__(self):
         """An official String representation of the vertice object"""
-        pass
+        return f'Graph: {self.graph}'
 
     def __len__(self):
         """Length of the graph"""
@@ -17,27 +17,37 @@ class Graph:
 
     def add_vert(self, val):
         """Add vertex as a value."""
-        if val not in self.graph:
-            self.graph['val'] = val
+        if self.has_vert(val):
+            return 'Already has the vertex!'
+        self.graph[val] = {}
 
     def has_vert(self, val):
         """A helper method to check for existing vertices."""
-        for vertex in self.graph.items():
-            if val is vertex:
-                return True
+        if val in self.graph.keys():
+            return True
         return False
 
     def add_edge(self, v1, v2, weight):
         """Adds an edge when two vertices are present, edge has a weight."""
-
+        if self.has_vert(v1) is False:
+            return 'No Vertex found'
         if v2 not in self.graph[v1]:
-            self.graph[v1].update({v2: weight})
+            self.graph[v1][v2] = weight
         else:
-            raise Exception
+            return 'Edge already exist'
 
     def get_neighbors(self, val):
-        """"""
-        # if val is self.graph:
+        """Gets the neighbors of that vertex"""
+        if self.has_vert(val) is False:
+            return 'No Vertex found'
+        elif self.graph[val].keys():
+            n = []
+
+            for k in self.graph[val].keys():
+                n.append(k)
+            return n
+        else:
+            return 'No neighbors found.'
 
 
 
