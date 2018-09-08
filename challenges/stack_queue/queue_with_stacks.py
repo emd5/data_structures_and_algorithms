f0de0cefd2738f8d507e8af0fa610aeffa4facca
emd5 <emahoney5@mail.greenriver.edu>
2018-09-08 11:07:27 -0700

    Resubmit Binary tree implementation, fix docstrings, and add more test cases

--- challenges/stack_queue/queue_with_stacks.py
+++ challenges/stack_queue/queue_with_stacks.py
@@ -7,16 +7,14 @@ stack2 = Stack()
 
 
 def enqueue(self, val):
-    """This method inserts value into the Queue using FIFO using stacks push pop methods. """
+    """This method inserts value into the Queue using FIFO using stacks push pop methods."""
     node = Node(val, self.top)
     stack1.push(node)
     return self.top
 
 
 def dequeue():
-    """This method extracts a value from the queue using FIFO using stacks push pop methods.
-    :return:
-    """
+    """This method extracts a value from the queue using FIFO using stacks push pop methods. """
     if stack2.top is not None:
         return stack2.pop()
     while stack1.top is not None:
--- data_structures/binary_tree/test_bst.py
+++ data_structures/binary_tree/test_bst.py
@@ -1,68 +1,81 @@
 from .bst import BinaryTree
 import pytest
 
+@pytest.fixture
+def empty_tree():
+    """Set up an empty tree."""
+    no_tree = BinaryTree([])
+    return no_tree
 
-def test_empty_bt():
-    """Test an instance of an empty binary tree. """
-    bt = BinaryTree()
-    assert isinstance(bt, BinaryTree)
-    assert bt.root is None
+
+@pytest.fixture
+def small_tree():
+    """Set up a small binary tree."""
+    small_bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
+    return small_bt
 
 
-def test_insert_into_empty_bt():
+def test_iterable_creates_bt(small_tree):
+    """Test iterable works when creating a small tree."""
+    assert small_tree.root.value == 20
+    assert small_tree.root.left.value == 18
+    assert small_tree.root.right.value == 40
+
+
+def test_empty_bt(empty_tree):
+    """Test an instance of an empty binary tree."""
+    assert isinstance(empty_tree, BinaryTree)
+    assert empty_tree.root is None
+
+
+def test_insert_into_empty_bt(empty_tree):
     """Test insert one node to a tree. """
-    bt = BinaryTree()
-    assert bt.root is None
-    bt.insert(25)
-    assert bt.root.value == 25
+    assert empty_tree.root is None
+    empty_tree.insert(25)
+    assert empty_tree.root.value == 25
 
 
-def test_iterable_creates_bt():
-    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
-    assert bt.root.value == 20
-    assert bt.root.left.value == 18
-    assert bt.root.right.value == 40
-
-
-def test_in_order_traversal():
-    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
-    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
-    actual = []
-
-    def generate_list(node):
-        actual.append(node.value)
-
-    bt.in_order(generate_list)
-    assert expected == actual
-
-
-def test_pre_order_traversal():
-    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
-    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
-    actual = []
-
-    def generate_list(node):
-        actual.append(node.value)
-
-    bt.in_order(generate_list)
-    assert expected == actual
-
-
-def test_post_order_traversal():
-    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
-    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
-    actual = []
-
-    def generate_list(node):
-        actual.append(node.value)
-
-    bt.in_order(generate_list)
-    assert expected == actual
-
-
-def test_insert_value_that_already_exists():
-    bt = BinaryTree([25])
+def test_insert_a_duplicate_value(small_tree):
+    """Test insert an existing value in the binary tree,
+    returns value error. """
     with pytest.raises(ValueError):
-        bt.insert(25)
+        small_tree.insert(14)
+
+
+def test_in_order_traversal(small_tree):
+    """Test all values are in ascending order."""
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    small_tree.in_order(generate_list)
+    assert expected == actual
+
+
+def test_pre_order_traversal(small_tree):
+    """Test all values are in pre-order traversal."""
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    small_tree.in_order(generate_list)
+    assert expected == actual
+
+
+def test_post_order_traversal(small_tree):
+    """Test all values are in post- order traversal."""
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    small_tree.in_order(generate_list)
+    assert expected == actual
+
 
 
