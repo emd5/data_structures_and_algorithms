6cea341cfa2a2722a28a3f98b7a9db27b9451cab
emd5 <emahoney5@mail.greenriver.edu>
2018-09-25 22:26:02 -0700

    did not finish implementing hashtable test

---
+++ challenges/tree_intersection/README.md
@@ -0,0 +1,10 @@
+# Intersection of binary trees
+Find common values in 2 binary trees.
+
+## Challenge
+Write a function called tree_intersection that takes two binary tree parameters.
+return a set of values found in both trees.
+
+## Solution
+<!-- Embedded whiteboard image -->
+
--- data_structures/hash_tables/hash_table.py
+++ data_structures/hash_tables/hash_table.py
@@ -1,19 +1,52 @@
-class Hashtable:
-    def __init__(self):
-        pass
+class HashTable:
+    """A class for a hash table."""
+    entries_count = 0
+    alphabet_size = 52
+
+    def __init__(self, size=8192):
+        self.table_size = size
+        self.hashtable = []
 
     def __repr__(self):
-        return f'Hash Table:'
+        return f'Hashtable : {self.hashtable} {self.table_size}'
 
-    def __str__(self):
-        return f'Hash Table:'
+    def _hash_key(self, key):
+        """Create a hash from a given key.
+        args:
+            key: a string to hash
+        returns: an integer corresponding to hashtable index
+        """
+        hash_ = 0
+        for i, c in enumerate(key):
+            hash_ += pow(
+                self.alphabet_size, len(key) - i - 1) * ord(c)
+        return hash_ % self.table_size
 
-    def put(self, key, value):
-        pass
+    def set(self, key, value):
+        """Add a key and value into the hash table.
+        args:
+            key: the key to store
+            value: the value to store
+        """
+        hash_key = self._hash_key(key)
+        if not self.hashtable[hash_key]:
+            self.hashtable[hash_key] = value
+            return True
+        return False
 
-    def get(self,key):
-        pass
+    def get(self, key):
+        """Retrieve a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        return self.hashtable[key]
 
     def remove(self, key):
-        pass
-
+        """Retrieve and remove a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        if self.hashtable[key]:
+            self.hashtable.remove(key)
