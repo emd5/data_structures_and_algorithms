6529d12e3f4e079c14a0f67c328178f83d86a17f
emd5 <emahoney5@mail.greenriver.edu>
2018-09-26 23:07:34 -0700

    create a test methods, update readme with whiteboard image, will complete after hash table class is implemented

---
+++ data_structures/left_join/left_join.py
@@ -0,0 +1,3 @@
+def left_join(ht1, ht2):
+    """This method accepts two hash tables and left joins two tables, returns a list of left join tables"""
+    pass
---
+++ data_structures/left_join/test_left_join.py
@@ -0,0 +1,23 @@
+from .left_join import left_join
+import pytest
+
+
+@pytest.fixture
+def setup1():
+    """Setup hashtable 1"""
+    pass
+
+
+def setup2():
+    """Setup hashtable 2"""
+    pass
+
+
+def test_left_join_method_exist():
+    assert left_join
+
+
+def test_empty_hash_table_exist():
+    pass
+
+
