602cc00446e6377ef6772803600cadf1191d3998
emd5 <emahoney5@mail.greenriver.edu>
2018-09-26 22:43:17 -0700

    add and update hashtable class, will continue to work on this

--- data_structures/hash_tables/test_hash_table.py
+++ data_structures/hash_tables/test_hash_table.py
@@ -3,20 +3,48 @@ import pytest
 
 
 @pytest.fixture
-def set_up_hash_table():
+def empty_hash_table():
+    """Setup empty hash table"""
     hash_table = HashTable()
-    h = [None] * hash_table.table_size
-    return h
+    return hash_table
 
 
-def test_size_hash_table(set_up_hash_table):
-    assert set_up_hash_table == 8192
+@pytest.fixture
+def setup_hash_table():
+    """Setup hash table with key and value"""
+    hash_table = HashTable()
+    hash_table.set('Apple', 'Jack')
+    hash_table.set('Joe', 'Schmoe')
+    return hash_table
 
 
 def test_hash_table_class_exist():
+    """Test HashTable class exist"""
     assert HashTable
 
 
-def test_hash_table_is_set_successful(set_up_hash_table):
-    assert set_up_hash_table.set('A', 1) is True
+def test_size_hash_table():
+    """Test length of hash table is 8192"""
+    hash_table = HashTable()
+    assert hash_table.table_size == 8192
+
+
+def test_hash_table_is_set_successful():
+    """Test add key and value to hash table is true"""
+    hash_table = HashTable()
+    assert hash_table.set('Apple', 'Jack') is True
+
+
+def test_hash_table_add_same_existing_key():
+    """Test add another key and value to hash table is true"""
+    hash_table = HashTable()
+    assert hash_table.set('Apple', 'Jack') is True
+    assert hash_table.set('Apple', 'Jack') is True
+
+
+def test_get_key_exists(setup_hash_table):
+    """Test for key in hash table exist"""
+    assert setup_hash_table.get('Apple') == 'Jack'
+
+
 
