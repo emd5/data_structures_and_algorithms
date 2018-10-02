36039da5f1821574fd89977bfb28a4c5135653cc
emd5 <emahoney5@mail.greenriver.edu>
2018-10-01 22:24:55 -0700

    implemented mergesort method with passed tests

---
+++ data_structures/left_join/hash_table.py
@@ -0,0 +1,75 @@
+class HashTable:
+    """A class for a hash table."""
+    entries_count = 0
+    alphabet_size = 52
+
+    def __init__(self, size=8):
+        self.table_size = size
+        self.hashtable = [None] * self.table_size
+
+    def __repr__(self):
+        return f'Hashtable : {self.hashtable}'
+
+    def _hash_key(self, key):
+        """Create a hash from a given key.
+        args:
+            key: a string to hash
+        returns: an integer corresponding to hashtable index
+        """
+        hash_ = 0
+        for i, c in enumerate(key):
+            hash_ += pow(self.alphabet_size, len(key) - i - 1) * ord(c)
+        return hash_ % self.table_size
+
+    def set(self, key, value):
+        """Add a key and value into the hash table.
+        args:
+            key: the key to store
+            value: the value to store
+        """
+
+        hash_key = self._hash_key(key)
+        key_value = [key, value]
+
+        if self.hashtable[hash_key] is None:
+            self.hashtable[hash_key] = list([key_value])
+            return True
+        else:
+            for pair in self.hashtable[hash_key]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.hashtable[hash_key].append(key_value)
+            return True
+
+    def get(self, key):
+        """Retrieve a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        hash_key = self._hash_key(key)
+        if self.hashtable[hash_key] is not None:
+            for pair in self.hashtable[hash_key]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    def remove(self, key):
+        """Retrieve and remove a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        hash_key = self._hash_key(key)
+
+        if self.hashtable[hash_key] is None:
+            return False
+        for i in range(0, len(self.hashtable[hash_key])):
+            if self.hashtable[hash_key][i][0] == key:
+                self.hashtable[hash_key].pop(i)
+                return True
+        return False
+
+
+
--- data_structures/left_join/test_left_join.py
+++ data_structures/left_join/test_left_join.py
@@ -1,16 +1,32 @@
 from .left_join import left_join
+from .hash_table import HashTable
 import pytest
 
 
 @pytest.fixture
+def empty_hash_table():
+    empty_ht = HashTable()
+    return empty_ht
+
+
+@pytest.fixture
 def setup1():
-    """Setup hashtable 1"""
-    pass
+    """Setup hash table 1"""
+    ht1 = HashTable()
+    ht1.set('Apple', 'Jack')
+    ht1.set('Pinky', 'Pie')
+    ht1.set('Flutter', 'Shy')
+    return ht1
 
 
+@pytest.fixture
 def setup2():
-    """Setup hashtable 2"""
-    pass
+    """Setup hash table 2"""
+    ht2 = HashTable()
+    ht2.set('Apple', 'Dash')
+    ht2.set('Flutter', 'Sparkle')
+    ht2.set('Pinky', 'Pony')
+    return ht2
 
 
 def test_left_join_method_exist():
@@ -18,6 +34,9 @@ def test_left_join_method_exist():
 
 
 def test_empty_hash_table_exist():
-    pass
+    assert empty_hash_table
 
 
+def test_left_join_method_is_successful(setup1, setup2):
+    """Test hash table 2 left joins with hash table 1."""
+    assert left_join(setup1, setup2) == [['Apple', 'Jack']]
